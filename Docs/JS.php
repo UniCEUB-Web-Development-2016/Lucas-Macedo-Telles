3559bbbb17e77bccdd01783d516e170b95b1cbb6
lucasdimatteu <lucasmdimatteu@gmail.com>
2016-03-16 14:04:22 -0300

    JS
    Eu não consegui puxar o script que fiz no LayoutManager. No LayoutManager eu teria que criar o LayoutController?

---
+++ Docs/JS.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+    function Name(name) {
+        return name;
+    }
+    function LastName(lastname) {
+        return lastname;
+    }
+    function Email(email) {
+        return email;
+    }
+    function Password(pass) {
+        return pass;
+    }
+    function BirthDate(date) {
+        return date;
+    }
+</script>
+
+</body>
+</html>
