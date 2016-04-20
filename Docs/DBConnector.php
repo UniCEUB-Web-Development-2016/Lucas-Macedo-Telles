e91af44a8542a2dc0db525ca09330708c21b9cad
lucasdimatteu <lucasmdimatteu@gmail.com>
2016-04-19 22:17:50 -0300

    Added files via upload

---
+++ Docs/DBConnector.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class DBConnector
+{
+    private $ipAddr;
+    private $dbName;
+    private $dbType;
+    private $dbPort;
+    private $dbUser;
+    private $dbPassword;
+	
+  
+    public function connection(){
+        $dbh = new PDO('mysql:host='.$this->getIpAddr().';port='.$this.$this->getDbPort().';dbname='.$this->getDbName());
+        return $dbh;
+    }
+ }
